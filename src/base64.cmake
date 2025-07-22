bc7aff8093602a44e3ee3b48b6974970233637f6
Sheldon Robinson <sheldon.robinson@gmail.com>
2025-07-22 12:39:07 -0400

    Removed component base64

--- src/base64.cmake
+++ src/base64.cmake
@@ -1,6 +1,6 @@
 add_library(docwire_base64 SHARED base64.cpp)
 
-find_library(base64 base64 REQUIRED)
+# find_library(base64 base64 REQUIRED)
 target_link_libraries(docwire_base64 PRIVATE ${base64} docwire_core)
 find_path(base64_inc_dir libbase64.h REQUIRED)
 target_include_directories(docwire_base64 PRIVATE ${base64_inc_dir})
